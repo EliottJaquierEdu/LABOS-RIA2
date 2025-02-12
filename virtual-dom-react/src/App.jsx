02c43e76f9ca28cd39cad47dffd1dc020242e26d
eliott <eliott.jaquier@gmail.com>
2025-02-12 16:03:47 +0100

    feat(virtual-dom-react): Add counter component in App

--- virtual-dom-react/src/App.jsx
+++ virtual-dom-react/src/App.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import Counter from "./components/Counter";
 
 function App() {
     return (
         <div className="app">
             <h1>React Counter App</h1>
-	//TODO
+            <Counter />
         </div>
     );
 }
