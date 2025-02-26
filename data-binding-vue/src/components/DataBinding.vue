dabb4dced5ebb7b125a09a347b66d47ee78576eb
eliott <eliott.jaquier@gmail.com>
2025-02-26 15:45:20 +0100

    feat: implement data binding in DataBinding component with sample JSON data

--- data-binding-vue/src/components/DataBinding.vue
+++ data-binding-vue/src/components/DataBinding.vue
@@ -1,8 +1,18 @@
 <script setup>
-
+const jsonData = {
+  name: "John Doe",
+  items: ["Item 1", "Item 2", "Item 3", "Item 4"]
+};
 </script>
 
-<template></template>
+<template>
+  <div id="name-container">
+    <p>{{ jsonData.name }}</p>
+  </div>
+  <ul>
+    <li v-for="item in jsonData.items">{{ item }}</li>
+  </ul>
+</template>
 
 <style scoped>
 
